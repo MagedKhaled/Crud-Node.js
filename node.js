bd06f94f1e479ec0196edad07a4b01b80a28ec01
MagedKhaled <32371968+MagedKhaled@users.noreply.github.com>
2023-12-06 09:59:47 +0200

    remove consoles

--- node.js
+++ node.js
@@ -24,34 +24,22 @@ let savedData = [
 
 
 app.get('/',(req,res) => {
-    console.log(savedData)
     return res.send(savedData);
 })
 
 app.delete('/:id/',(req,res)=>{
-    console.log(req.params.id)
-    console.log(savedData)
-    // savedData.find(x => x.id == req.params.id)
     savedData = savedData.filter((product) => product.id != req.params.id)
     return res.send(savedData)
 })
 
 app.put('/',(req,res)=>{
-    console.log('put')
-    console.log(req.body)
     let index = savedData.findIndex(x => x.id === req.body.id)
-    console.log(savedData)
-    console.log(index)
     savedData[index].Name = req.body.name
     savedData[index].Cost = req.body.cost
-    console.log(savedData)
     return res.send(savedData)
 })
 
 app.post('/',(req,res) => {
-    console.log('post')
-    console.log(req.body)
-    console.log(savedData)
     savedData.push({id:savedData.length,Name:req.body.name,Cost:req.body.cost})
 
     return res.send(savedData);
